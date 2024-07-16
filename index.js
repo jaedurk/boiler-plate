42d8977cd03eefacdd7ab576af523bc77f020e11
jdlee <jdlee@jasongroup.co.kr>
2024-07-16 16:41:05 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require('express');
+const app = express();
+const port = 4000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+//application/x-www/form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://jdlee:1004dang@cluster0.nwrvbju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+mongoose.connect(config.mongoURI, {
     // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => {
     console.log('MongoDB Connected...')
@@ -11,6 +22,16 @@ mongoose.connect("mongodb+srv://jdlee:1004dang@cluster0.nwrvbju.mongodb.net/?ret
 
 app.get('/', (req, res) => {
     res.send('Hello World! 안녕하세요')
+})
+
+app.post('/register', (req, res) => {
+    //회원가입할때 필요한 정보들을 client에서 가져요면 그것들을 데이터베이스에 넣어준다
+    const user = new User(req.body);
+    user.save().then(() => {
+       res.status(200).json({ success: true });
+    }).catch((err) => {
+        res.json({success: false, err});
+    });
 })
 
 app.listen(port, () => {
